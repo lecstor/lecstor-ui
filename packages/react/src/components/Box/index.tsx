a50a7eb05d4fc0b8d84abcfc6600dba413811753
Jason Galea <jason@lecstor.com>
2020-10-11 23:11:05 +1000

    Box: fix & extend styling

--- packages/react/src/components/Box/index.tsx
+++ packages/react/src/components/Box/index.tsx
@@ -1,28 +1,64 @@
-import { css, jsx } from "@emotion/react";
-import { FC } from 'react';
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+import isPropValid from "@emotion/is-prop-valid";
+import { FC } from "react";
 import {
   border,
   color,
+  flexbox,
   layout,
   space,
+  typography,
   BorderProps,
+  FlexboxProps,
   SpaceProps,
   ColorStyleProps,
   LayoutProps,
+  TypographyProps,
   compose,
-} from 'styled-system';
+} from "styled-system";
+
+function filterProps(props: any) {
+  const htmlProps: Record<string, string> = {};
+  Object.keys(props).forEach((p) => {
+    if (isPropValid(p)) {
+      htmlProps[p] = props[p];
+    }
+  });
+  return htmlProps;
+}
 
 type SxFnProps = BorderProps &
+  FlexboxProps &
   SpaceProps &
   ColorStyleProps &
-  LayoutProps & { className?: string; as?: string };
+  LayoutProps &
+  TypographyProps & { className?: string; as?: string };
 
 export type BoxProps = SxFnProps;
 
-export const Box: FC<BoxProps> = ({ children, className, as = 'div', ...props }) => {
+export const Box: FC<BoxProps> = ({
+  children,
+  className,
+  as = "div",
+  ...props
+}) => {
   return jsx(as, {
     className,
-    css: css(compose(border, color, layout, space)(props)),
+    css: (theme: any) =>
+      compose(
+        () => ({
+          boxSizing: "border-box",
+          minWidth: 0,
+        }),
+        border,
+        color,
+        flexbox,
+        layout,
+        space,
+        typography
+      )({ theme, ...props }),
+    ...filterProps(props),
     children,
   });
 };
