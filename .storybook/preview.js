a65e2860411c9e4483a48959bb585495bf87b341
Jason Galea <jason@lecstor.com>
2020-10-11 23:08:01 +1000

    storybook - add web font & theme provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import React from 'react';
+import { DsProvider } from '../packages/react/src/theme';
+
+export const decorators = [
+  (Story) => (
+    <DsProvider>
+      <Story />
+    </DsProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
